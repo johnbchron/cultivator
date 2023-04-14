de5b14ce50ac573afbaa373e0766a8835b3ca063
John Lewis <johnbchron@outlook.com>
2023-04-14 00:16:51 -0500

    dump

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,80 @@
+mod constants;
+mod hex;
+
+use constants::*;
+use hex::HexItem;
+
 use bevy::{
+  app::{App, PluginGroup},
+  asset::{AssetServer, Assets, Handle},
+  core_pipeline::{core_2d::Camera2dBundle, core_3d::Camera3dBundle},
   diagnostic::{
     EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin,
     LogDiagnosticsPlugin,
   },
-  prelude::*,
-  render::{
-    camera::ScalingMode, mesh::Indices, render_resource::PrimitiveTopology,
+  ecs::{
+    component::Component,
+    query::{Changed, With},
+    system::{Commands, Query, Res, ResMut, Resource},
+    world::{FromWorld, World},
   },
+  input::{keyboard::KeyCode, Input},
+  math::{Quat, UVec2, Vec3},
+  pbr::{DirectionalLightBundle, PbrBundle, StandardMaterial},
+  render::{
+    camera::{
+      Camera, OrthographicProjection, Projection, RenderTarget, ScalingMode,
+      Viewport,
+    },
+    mesh::{Indices, Mesh},
+    render_resource::{
+      Extent3d, PrimitiveTopology, TextureDescriptor, TextureDimension,
+      TextureFormat, TextureUsages,
+    },
+    texture::{BevyDefault, Image, ImagePlugin},
+    view::{Msaa, RenderLayers},
+  },
+  sprite::SpriteBundle,
+  time::Time,
+  transform::components::Transform,
+  utils::default,
+  window::{Window, WindowPlugin},
+  DefaultPlugins,
 };
 use bevy_diagnostic_vertex_count::{
   VertexCountDiagnosticsPlugin, VertexCountDiagnosticsSettings,
 };
+
 use hexx::*;
-use rand::Rng;
+
 use std::collections::HashMap;
+// for enum to vec
 use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
-
-const HEX_SIZE: Vec2 = Vec2::splat(1.0);
-const HEX_HEIGHT: f32 = 1.0;
-const PAN_SPEED: f32 = 50.0;
-const ZOOM_SPEED: f32 = 1.0;
-const UNIT_TO_PIXEL: f32 = 50.0;
 
 #[derive(Component)]
-struct Player;
-
-#[derive(Component)]
-#[allow(dead_code)]
-#[derive(EnumIter, Eq, Hash, PartialEq, Clone, Copy)]
-enum GridItem {
-  Dirt,
-  Grass,
-  Stone,
-  Cobblestone,
-}
-
-impl GridItem {
-  fn random() -> Self {
-    let mut rng = rand::thread_rng();
-    match rng.gen_range(0..4) {
-      0 => Self::Dirt,
-      1 => Self::Grass,
-      2 => Self::Stone,
-      3 => Self::Cobblestone,
-      _ => unreachable!(),
-    }
-  }
-  fn hex_height(&self) -> f32 {
-    match self {
-      Self::Dirt => HEX_HEIGHT,
-      Self::Grass => HEX_HEIGHT + 0.2,
-      Self::Stone => HEX_HEIGHT + 0.3,
-      Self::Cobblestone => HEX_HEIGHT + 0.4,
-    }
-  }
-  fn base_color(&self) -> Color {
-    match self {
-      Self::Dirt => Color::rgb(0.5, 0.3, 0.1),
-      Self::Grass => Color::rgb(0.0, 0.5, 0.0),
-      // Self::Grass => Color::rgb(1.0, 1.0, 1.0),
-      Self::Stone => Color::rgb(0.5, 0.5, 0.5),
-      Self::Cobblestone => Color::rgb(0.3, 0.3, 0.3),
-    }
-  }
-  fn load_texture(&self, asset_server: &AssetServer) -> Option<Handle<Image>> {
-    match self {
-      Self::Dirt => None,
-      // Self::Grass => Some(asset_server.load("textures/grass.png")),
-      Self::Grass => None,
-      Self::Stone => None,
-      Self::Cobblestone => None,
-    }
-  }
-
-  fn build_material(&self, asset_server: &AssetServer) -> StandardMaterial {
-    StandardMaterial {
-      base_color: self.base_color(),
-      base_color_texture: match self.load_texture(asset_server) {
-        Some(texture) => Some(texture),
-        None => StandardMaterial::default().base_color_texture,
-      },
-      metallic: match self {
-        Self::Dirt => 0.0,
-        Self::Grass => 0.0,
-        Self::Stone => 0.5,
-        Self::Cobblestone => 0.5,
-      },
-      perceptual_roughness: match self {
-        Self::Dirt => 0.8,
-        Self::Grass => 0.8,
-        Self::Stone => 0.4,
-        Self::Cobblestone => 0.5,
-      },
-      // reflectance: ??,
-      ..Default::default()
-    }
-  }
-}
-
-#[derive(Component)]
-struct GridPosition(Hex);
+struct HexPosition(Hex);
 
 #[derive(Resource)]
-struct HexMaterials(HashMap<GridItem, Handle<StandardMaterial>>);
+struct HexMaterials(HashMap<HexItem, Handle<StandardMaterial>>);
 
 #[derive(Resource)]
-struct HexMeshes(HashMap<GridItem, Handle<Mesh>>);
+struct HexMeshes(HashMap<HexItem, Handle<Mesh>>);
 
 #[derive(Resource)]
 struct RenderSettings {
   frac_resolution: f32,
 }
 
+#[derive(Component)]
+struct PixelCamera;
+
+#[derive(Component)]
+struct PixelCanvas;
+
 impl FromWorld for HexMaterials {
   fn from_world(world: &mut World) -> Self {
     let mut map = HashMap::new();
-    for item in GridItem::iter() {
+    for item in HexItem::iter() {
       let asset_server = world.get_resource::<AssetServer>().unwrap();
       let material = item.build_material(asset_server);
       let mut materials = world
@@ -132,7 +90,7 @@ impl FromWorld for HexMeshes {
   fn from_world(world: &mut World) -> Self {
     let mut meshes = world.get_resource_mut::<Assets<Mesh>>().unwrap();
     let mut map = HashMap::new();
-    for item in GridItem::iter() {
+    for item in HexItem::iter() {
       map.insert(item, meshes.add(build_hex_mesh(item.hex_height())));
     }
     Self(map)
@@ -142,7 +100,7 @@ impl FromWorld for HexMeshes {
 impl Default for RenderSettings {
   fn default() -> Self {
     Self {
-      frac_resolution: 0.125,
+      frac_resolution: INITIAL_PIXEL_SCALE.recip(),
     }
   }
 }
@@ -163,11 +121,11 @@ fn build_hex_mesh(height: f32) -> Mesh {
 }
 
 fn build_hex_bundle(
-  grid_item: &GridItem,
+  grid_item: &HexItem,
   hex: &Hex,
   hex_materials: &HexMaterials,
   hex_meshes: &HexMeshes,
-) -> (PbrBundle, GridPosition, GridItem) {
+) -> (PbrBundle, HexPosition, HexItem) {
   let hex_layout = HexLayout {
     hex_size: HEX_SIZE,
     ..Default::default()
@@ -185,7 +143,7 @@ fn build_hex_bundle(
       .looking_to(Vec3::Z, Vec3::Y),
       ..Default::default()
     },
-    GridPosition(*hex),
+    HexPosition(*hex),
     *grid_item,
   )
 }
@@ -202,7 +160,7 @@ fn build_test_grid(
     let r2 = (n - q).min(n);
     for r in r1..r2 {
       let hex = Hex::new(q, r);
-      let item = GridItem::random();
+      let item = HexItem::random();
       grid.insert(hex, item);
     }
   }
@@ -212,45 +170,179 @@ fn build_test_grid(
   }
 }
 
-fn setup_graphics(mut commands: Commands) {
-  commands.spawn(Camera3dBundle {
-    transform: Transform::from_translation(Vec3::new(0.0, 20.0, 20.0))
-      .looking_at(Vec3::ZERO, Vec3::Y)
-      .with_translation(Vec3::new(
-        0.0,
-        500.0 + 20.0 + HEX_HEIGHT,
-        500.0 + 20.0,
-      )),
-    projection: Projection::Orthographic(OrthographicProjection {
-      scale: 1.0,
-      scaling_mode: ScalingMode::WindowSize(UNIT_TO_PIXEL),
-      ..Default::default()
-    }),
-    ..Default::default()
-  });
-  // spawn light
+fn setup_graphics(
+  mut commands: Commands,
+  windows: Query<&Window>,
+  render_settings: Res<RenderSettings>,
+  mut images: ResMut<Assets<Image>>,
+) {
+  // spawn lighting
   commands.spawn(DirectionalLightBundle {
     transform: Transform::from_translation(Vec3::new(0.0, 100.0, 0.0))
       .looking_to(Vec3::NEG_Y, Vec3::Z),
     ..default()
   });
+
+  // first pass output size
+  let window = windows.iter().next().unwrap();
+  let window_size = Extent3d {
+    width: window.resolution.width() as u32,
+    height: window.resolution.height() as u32,
+    ..Default::default()
+  };
+  let scaled_size = Extent3d {
+    width: (window_size.width as f32 * render_settings.frac_resolution) as u32,
+    height: (window_size.height as f32 * render_settings.frac_resolution)
+      as u32,
+    ..Default::default()
+  };
+
+  // image for first pass output
+  let mut first_pass_output = Image {
+    texture_descriptor: TextureDescriptor {
+      label: None,
+      size: scaled_size,
+      dimension: TextureDimension::D2,
+      format: TextureFormat::bevy_default(),
+      mip_level_count: 1,
+      sample_count: 1,
+      usage: TextureUsages::TEXTURE_BINDING
+        | TextureUsages::COPY_DST
+        | TextureUsages::RENDER_ATTACHMENT,
+      view_formats: &[],
+    },
+    ..Default::default()
+  };
+
+  // fill first pass buffer with zeroes
+  first_pass_output.resize(scaled_size);
+
+  let first_pass_output_handle = images.add(first_pass_output);
+
+  let isometric_rotation = Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4);
+
+  // first pass camera
+  commands.spawn((
+    Camera3dBundle {
+      transform: Transform::from_translation(Vec3::new(0.0, 20.0, 20.0))
+        .with_rotation(isometric_rotation)
+        .with_translation(Vec3::new(
+          0.0,
+          500.0 + 20.0 + HEX_HEIGHT,
+          500.0 + 20.0,
+        )),
+      projection: Projection::Orthographic(OrthographicProjection {
+        scale: 1.0 / render_settings.frac_resolution,
+        scaling_mode: ScalingMode::WindowSize(UNIT_TO_PIXEL),
+        ..Default::default()
+      }),
+      camera: Camera {
+        target: RenderTarget::Image(first_pass_output_handle.clone()),
+        viewport: Some(Viewport {
+          physical_size: UVec2 {
+            x: scaled_size.width,
+            y: scaled_size.height,
+          },
+          ..Default::default()
+        }),
+        ..Default::default()
+      },
+      ..Default::default()
+    },
+    PixelCamera,
+  ));
+
+  let post_processing_pass_layer =
+    RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8);
+
+  // the sprite that the image from the first pass will be displayed on.
+  commands.spawn((
+    SpriteBundle {
+      // mesh: quad_handle,
+      texture: first_pass_output_handle.clone().into(),
+      transform: Transform::from_translation(Vec3::ZERO).with_scale(Vec3::new(
+        render_settings.frac_resolution.recip(),
+        render_settings.frac_resolution.recip(),
+        0.0,
+      )),
+      ..Default::default()
+    },
+    PixelCanvas,
+    post_processing_pass_layer,
+  ));
+
+  // The post-processing pass camera.
+  commands.spawn((
+    Camera2dBundle {
+      camera: Camera {
+        // renders after the first main camera which has default value: 0.
+        order: 1,
+        ..default()
+      },
+      ..Camera2dBundle::default()
+    },
+    post_processing_pass_layer,
+  ));
 }
 
-// // edit the camera viewport to maintain fractional pixel resolution
-// fn maintain_fractional_camera_resolution(
-// 	mut query: Query<&mut Camera>,
-// 	render_settings: Res<RenderSettings>,
-// ) {
-// 	for mut camera in query.iter_mut() {
-// 		let mut viewport = camera.get_viewport();
-// 		viewport.scale = render_settings.frac_resolution;
-// 		camera.set_viewport(viewport);
-// 	}
-// }
+// edit the camera viewport to maintain fractional pixel resolution
+fn maintain_fractional_camera_resolution(
+  mut cameras: Query<&mut Camera, With<PixelCamera>>,
+  mut canvases: Query<&mut Handle<Image>, With<PixelCanvas>>,
+  windows: Query<&Window, Changed<Window>>,
+  render_settings: Res<RenderSettings>,
+  mut images: ResMut<Assets<Image>>,
+) {
+  match windows.iter().next() {
+    Some(window) => {
+      let scaled_size = Extent3d {
+        width: (window.resolution.width() as f32
+          * render_settings.frac_resolution) as u32,
+        height: (window.resolution.height() as f32
+          * render_settings.frac_resolution) as u32,
+        ..Default::default()
+      };
+
+      let mut camera = cameras.iter_mut().next().unwrap();
+      let mut canvas = canvases.iter_mut().next().unwrap();
+
+      let mut new_first_pass_target = Image {
+        texture_descriptor: TextureDescriptor {
+          label: None,
+          size: scaled_size,
+          dimension: TextureDimension::D2,
+          format: TextureFormat::bevy_default(),
+          mip_level_count: 1,
+          sample_count: 1,
+          usage: TextureUsages::TEXTURE_BINDING
+            | TextureUsages::COPY_DST
+            | TextureUsages::RENDER_ATTACHMENT,
+          view_formats: &[],
+        },
+        ..Default::default()
+      };
+      new_first_pass_target.resize(scaled_size);
+
+      let new_first_pass_target_handle = images.add(new_first_pass_target);
+      *canvas = new_first_pass_target_handle.clone();
+      camera.target = RenderTarget::Image(new_first_pass_target_handle);
+
+      camera.viewport = Some(Viewport {
+        physical_size: UVec2 {
+          x: scaled_size.width,
+          y: scaled_size.height,
+        },
+        ..Default::default()
+      });
+    }
+    None => (),
+  }
+}
 
 fn handle_camera_movement(
   mut query: Query<(&mut Transform, &mut Projection), With<Camera>>,
   time: Res<Time>,
+  render_settings: Res<RenderSettings>,
   keyboard_input: Res<Input<KeyCode>>,
 ) {
   for (mut transform, projection) in query.iter_mut() {
@@ -274,7 +366,8 @@ fn handle_camera_movement(
       if keyboard_input.pressed(KeyCode::Q) {
         match projection {
           Projection::Orthographic(ref mut ortho) => {
-            ortho.scale += ZOOM_SPEED * time.delta_seconds();
+            ortho.scale += ZOOM_SPEED * time.delta_seconds()
+              / render_settings.frac_resolution;
           }
           _ => {
             unimplemented!()
@@ -284,7 +377,8 @@ fn handle_camera_movement(
       if keyboard_input.pressed(KeyCode::E) {
         match projection {
           Projection::Orthographic(ref mut ortho) => {
-            ortho.scale -= ZOOM_SPEED * time.delta_seconds();
+            ortho.scale -= ZOOM_SPEED * time.delta_seconds()
+              / render_settings.frac_resolution;
             // *projection = Projection::Orthographic(ortho);
           }
           _ => {
@@ -302,16 +396,22 @@ fn handle_camera_movement(
 
 fn main() {
   App::new()
-    .add_plugins(DefaultPlugins.set(WindowPlugin {
-      primary_window: Some(Window {
-      	present_mode: bevy::window::PresentMode::AutoNoVsync,
-				..Default::default()
-			}),
-			..Default::default()
-    }))
+    // default plugins with no vsync
+    .add_plugins(
+      DefaultPlugins
+        .set(WindowPlugin {
+          primary_window: Some(Window {
+            present_mode: bevy::window::PresentMode::AutoNoVsync,
+            ..Default::default()
+          }),
+          ..Default::default()
+        })
+        .set(ImagePlugin::default_nearest()),
+    )
     // graphics config
-    .insert_resource(Msaa::Sample2)
+    .insert_resource(Msaa::Off)
     .insert_resource(RenderSettings::default())
+    .add_system(maintain_fractional_camera_resolution)
     // diagnostic config
     .add_plugin(LogDiagnosticsPlugin::default())
     .add_plugin(FrameTimeDiagnosticsPlugin::default())
