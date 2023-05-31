329ce4803ad1539b9b4cb57fb6d1e3a215911c1c
John Lewis <johnbchron@outlook.com>
2023-05-31 01:09:57 -0500

    dump

--- planiscope/src/lib.rs
+++ planiscope/src/lib.rs
@@ -2,23 +2,36 @@
 use fidget::eval::{Family, Tape};
 use fidget::mesh::{Mesh, Octree, Settings};
 use fidget::rhai::eval;
-use fidget::jit;
+// use fidget::context::Node;
+use fidget::vm;
 
 pub struct MeshWrapper {
 	pub mesh: Mesh,
 	pub normals: Vec<[f32; 3]>,
 }
 
+// fn transform_viewport(root: Node, ctx: &mut fidget::context::Context, mat: [[f32; 4]; 4]) -> Node {
+//   let (x, y, z) = (ctx.x(), ctx.y(), ctx.z());
+//   let (x, y, z, w) = (
+//     mat[0][0] * x + mat[0][1] * y + mat[0][2] * z + mat[0][3],
+//     mat[1][0] * x + mat[1][1] * y + mat[1][2] * z + mat[1][3],
+//     mat[2][0] * x + mat[2][1] * y + mat[2][2] * z + mat[2][3],
+//     mat[3][0] * x + mat[3][1] * y + mat[3][2] * z + mat[3][3],
+//   );
+// }
+
 pub fn mesh_from_surface(expr: &str, depth: u8) -> MeshWrapper {
   let (surface, ctx) = eval(expr).unwrap();
-  let tape = ctx.get_tape::<jit::Eval>(surface).unwrap();
+  let transformed_surface = surface;
+  
+  let tape = ctx.get_tape::<vm::Eval>(transformed_surface).unwrap();
 
   let settings = Settings {
     threads: 8,
     min_depth: depth,
     max_depth: 0,
   };
-  let octree = Octree::build::<jit::Eval>(&tape, settings);
+  let octree = Octree::build::<vm::Eval>(&tape, settings);
   let mesh = octree.walk_dual(settings);
 
   // let normals = smooth_vertex_normals(&mesh);
